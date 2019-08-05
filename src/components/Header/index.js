84a6bf993ecf0b7aa0eac01fa67d4e731bdee73b
Cleyton Gama <cleytongama@gmail.com>
2019-08-04 22:20:03 -0300

    App layout pipefy

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Container} from './styles'
+function Header() {
+    return (
+        <Container>
+            <h1>App</h1>
+        </Container>
+    ) 
+}
+export default Header
